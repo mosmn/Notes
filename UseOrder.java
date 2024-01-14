0ee3ea28396cc7636c3fa5b71919c1fede059e19
mosmn <ithinkisee8@gmail.com>
2024-01-15 07:55:18 +0800

    Remove group information from UseOrder.java

--- UseOrder.java
+++ UseOrder.java
@@ -1,7 +1,3 @@
-// Group:
-// Moahamed Ahmed Mohamed Osman (SW01082449)
-// Abbas Mohammad Khaled Ali (SW01081578)
-
 public class UseOrder {
     public static void main(String[] args) {
         ShippedOrder shippedOrder = new ShippedOrder();
